adaff907b5bdf5050b269df4794b40b5cf83af37
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-06-29 12:47:23 +0300

    task 12 completed

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+str1 = "Fizz"
+str2 = "Buzz"
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if (number % 3 and number % 5):
+            print("%s%s" % (str1, str2), end=' ')
+        elif (number % 3):
+            print("%s" % (str1), end=' ')
+        elif (number % 5):
+            print("%s" % (str2), end=' ')
+        else:
+            print("%d" % (number), end=' ')
