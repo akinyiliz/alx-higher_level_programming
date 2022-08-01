2a105669e285c9c5bdf62f1d491ddecf6a018aec
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-08-01 13:07:23 +0300

    task 2 completed

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""defining is_same_class function"""
+
+def is_same_class(obj, a_class):
+    """returns true if object is an instance of specified class"""
+    return(type(obj) == a_class)
