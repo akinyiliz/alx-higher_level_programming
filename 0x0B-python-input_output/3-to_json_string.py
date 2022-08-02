86f5fbab3232bea7af595a64a032f6d917eae01c
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-08-02 12:53:17 +0300

    task 3 modified

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that returns the JSON representation of an object(string)"""
+"""Defines a function that returns the JSON representation of an object"""
 import json
 
 
