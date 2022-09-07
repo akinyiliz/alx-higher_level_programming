ad0dfe618cbbc8fe5ea02d282e6e091e47c69f09
akinyiliz <lizzieke84@gmail.com>
2022-09-07 13:33:47 +0300

    complete task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
--- Imports the database dump hbtn_0d_tvshows 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show
-INNER JOIN tv_show_genres
-ON tv_shows_id = tv_shows_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre.id;
+-- lists all shows contained in hbtn_0d_tvshows that have at
+-- least one genre linked
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+FROM `tv_shows`
+INNER JOIN `tv_show_genres`
+ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
