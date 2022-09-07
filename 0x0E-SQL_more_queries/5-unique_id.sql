8f4fc233f0f1520f14c7b385c10e0105937ff113
akinyiliz <lizzieke84@gmail.com>
2022-09-07 12:11:55 +0300

    complete task 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Create the table unique_id
-CREATE TABLE unique_id (id INT DEFAULT1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
