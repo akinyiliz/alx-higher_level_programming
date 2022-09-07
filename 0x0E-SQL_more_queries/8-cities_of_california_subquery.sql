faa9b326e7b445ca08c838499fbbcd614de09502
akinyiliz <lizzieke84@gmail.com>
2022-09-07 13:08:12 +0300

    complete task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,8 @@
 -- Lists all the cities of Carlifornia in the database hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id IN (
-	SELECT id FROM states WHERE name = "Carlifornia")
-ORDER BY id;
+SELECT id, name
+FROM cities
+WHERE state_id = (
+      SELECT id
+      FROM states
+      WHERE name = 'California'
+);
