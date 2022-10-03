f7244a2ced81af770d9840f7c6fb22a06f98de9a
akinyiliz <lizzieke84@gmail.com>
2022-10-03 18:01:39 +0300

    complete task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i = 0; i < array.length; i++){console.log(array[i]);}
+for (const array of ['C is fun', 'Python is cool', 'Javascript is amzing']) {
+	console.log(array);
+}
