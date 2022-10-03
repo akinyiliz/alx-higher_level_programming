65bd53542527b5e32a5ee6e2bcef954e00fab491
akinyiliz <lizzieke84@gmail.com>
2022-10-03 14:21:19 +0300

    complete task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const process = require('process');
+const count = require('process');
 if (process.argv.length === 2){console.log('No argument');}else if (process.argv.length === 3){console.log('Argument found');}else{console.log('Arguments found');}
