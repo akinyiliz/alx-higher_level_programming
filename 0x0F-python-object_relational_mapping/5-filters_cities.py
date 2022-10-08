2ae4c61aed56082fdebb2d6044b9dc1d19a28cae
akinyiliz <lizzieke84@gmail.com>
2022-10-08 13:24:25 +0300

    complete task 5

--- 0x0F-python-object_relational_mapping/5-filters_cities.py
+++ 0x0F-python-object_relational_mapping/5-filters_cities.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
 """
-A script that takes in the name of a state as an argument and lists
- all cities of that state, using the database hbtn_0e_4_usa
+Displays all cities of a given state from the
+states table of the database hbtn_0e_4_usa.
+Safe from SQL injections.
+Usage: ./5-filter_cities.py <mysql username> \
+                            <mysql password> \
+                            <database name> \
+                            <state name searched>
 """
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 5:
-        db = MySQLdb.connect(
-            host='localhost',
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-        )
-
-        cursor = db.cursor()
-        cursor.execute("""
-            SELECT cities.name FROM cities
-            INNER JOIN states
-            ON cities.state_id = states.id
-            WHERE CAST(states.name AS BINARY) = %s
-            ORDER BY cities.id;
-        """, [sys.argv[4]])
-        records = cursor.fetchall()
-        print(", ".join(map(lambda x: x[0], records)))
-        db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
