aa79559ed626cfd2f651f6b4abe7467c6eb64e40
akinyiliz <lizzieke84@gmail.com>
2022-10-21 16:26:09 +0300

    fix error

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-# Displays the response body for a given URL for 200 status code responses# curl -sL "$1"
+# Displays the response body for a given URL for 200 status code response
+curl -sL "$1"
